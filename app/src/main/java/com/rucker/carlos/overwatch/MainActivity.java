3259da8bae8f0fdfbf839f0e59cb2f2736a23a43
kenocvr <kenocvr@gmail.com>
2017-12-23 23:20:12 -0800

    Added Saved BattleTag. SharedPrefs.

--- app/src/main/java/com/rucker/carlos/overwatch/MainActivity.java
+++ app/src/main/java/com/rucker/carlos/overwatch/MainActivity.java
@@ -2,7 +2,9 @@ package com.rucker.carlos.overwatch;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -27,12 +29,16 @@ import retrofit2.Response;
 public class MainActivity extends Activity {
 
 
+    public static final String MyPREFERENCES = "myPrefs";
+    SharedPreferences sharedpreferences;
+
     private ProgressBar progress;
     public static final String EXTRA_MESSAGE = "com.rucker.carlos.overwatch.MESSAGE";
     private Button btnSubmit;
     private EditText battleTag;
     private EditText battleId;
     private TextView sampleTextView;
+    public TextView savedBattleTag;
 
     public String getBattleTag() {
         battleTag = (EditText) findViewById(R.id.BattleTag);
@@ -48,20 +54,91 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        savedBattleTag = findViewById(R.id.savedBattleTag);
         progress = findViewById(R.id.pd);
         progress.setVisibility(ProgressBar.INVISIBLE);
+        savedBattleTag();
         initViews();
+
+    }
+
+    private void savedBattleTag() {
+        SharedPreferences sp = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        final String dummyCheck = sp.getString("battleTagComplete",null);
+        if(dummyCheck != null){
+            savedBattleTag.setText(dummyCheck);
+            savedBattleTag.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                            /*SO META*/
+                    try{
+                        Client client = new Client();
+                        Service apiService = client
+                                .getClient()
+                                .create(Service.class);
+                        Call<Pojo> call = apiService.getJson(dummyCheck);
+
+                        call.enqueue(new Callback<Pojo>() {
+                            @Override
+                            public void onResponse(Call<Pojo> call, Response<Pojo> response) {
+                                GameStats____ dummy = response.body().getUs().getStats().getCompetitive().getGameStats();
+                                OverallStats____ overallStats = response.body().getUs().getStats().getCompetitive().getOverallStats();
+                                final String rank = overallStats.getComprank().toString();
+                                final String level = overallStats.getLevel().toString();
+                                final String winRate = overallStats.getWinRate().toString();
+                                final String tier = overallStats.getTier().toString();
+                                final String tierImage = overallStats.getTierImage().toString();
+                                final String avatar = overallStats.getAvatar().toString();
+                                final String damageTotal = dummy.getAllDamageDone().toString();
+                                final String eliminationsMostInGame = dummy.getEliminationsMostInGame().toString();
+
+
+                                progress = findViewById(R.id.pd);
+
+                                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                                intent.putExtra("DAMAGE_TOTAL", damageTotal);
+                                intent.putExtra("ELIMINATIONS_IN_GAME_TOTAL", eliminationsMostInGame);
+                                intent.putExtra("RANK",rank);
+                                intent.putExtra("WIN_RATE",winRate);
+                                intent.putExtra("TIER",tier);
+                                intent.putExtra("LEVEL",level);
+                                intent.putExtra("TIER_IMAGE",tierImage);
+                                intent.putExtra("AVATAR",avatar);
+
+
+
+                                startActivity(intent);
+
+                            }
+
+                            @Override
+                            public void onFailure(Call<Pojo> call, Throwable t) {
+                                Log.d("Error", t.getMessage());
+                                Toast.makeText(MainActivity.this, "Error Fetching Data!", Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+                    }catch (Exception e){
+                        Log.d("Error", e.getMessage());
+                        //Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                            /*SO META*/
+                }
+            });
+        }
+
     }
 
 
-
     private void initViews() {
+
+
         battleTag = (EditText) findViewById(R.id.BattleTag);
         battleId = (EditText) findViewById(R.id.BattleId);
-        sampleTextView = (TextView) findViewById(R.id.sampleTestFont);
+        //sampleTextView = (TextView) findViewById(R.id.sampleTestFont);
         Typeface owFont = Typeface.createFromAsset(getAssets(), "bignoodletoo.ttf");
         /*
-        *  HOW DO I CUTOMIZE TOOLBAR
+        *  HOW DO I CUTOMIZE ActionBar?
         */
         ActionBar ab = getActionBar();
 
@@ -89,7 +166,6 @@ public class MainActivity extends Activity {
 
         battleTag.setTypeface(owFont);
         battleId.setTypeface(owFont);
-        sampleTextView.setTypeface(owFont);
         btnSubmit = (Button) findViewById(R.id.btn_submit);
         btnSubmit.setTypeface(owFont);
 
@@ -102,9 +178,7 @@ public class MainActivity extends Activity {
         });
     }
 
-    public void activityStart(String extra){
 
-    }
 
     private void loadJson(){
         //battleTag = findViewById(R.id.BattleTag);
@@ -112,7 +186,7 @@ public class MainActivity extends Activity {
 
         String strBattleTag = battleTag.getText().toString();
         String strBattleId = battleId.getText().toString();
-        String battleTagComplete = strBattleTag+"-"+strBattleId;
+        final String battleTagComplete = strBattleTag+"-"+strBattleId;
         Log.d("BATTLETAGCOMPLETE::: ", battleTagComplete);
         try{
             Client client = new Client();
@@ -147,8 +221,19 @@ public class MainActivity extends Activity {
                     intent.putExtra("LEVEL",level);
                     intent.putExtra("TIER_IMAGE",tierImage);
                     intent.putExtra("AVATAR",avatar);
-                    startActivity(intent);
 
+                        // Duplicate object
+                    //sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                    editor.putString("battleTagComplete", battleTagComplete);
+
+                    editor.apply();
+
+                    savedBattleTag.setText(battleTagComplete);
+
+
+                    startActivity(intent);
 
                 }
 
